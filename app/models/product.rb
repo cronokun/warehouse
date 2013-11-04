5d220b38f6530356dc622a9393aafcc05fe1bd4b
Sergey Kviatkovsky <crono.neko@gmail.com>
2013-11-04 21:13:25 +0300

    Add validation for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :width, :height, :depth, :weight, :stock_level,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,56 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "new instance" do
+    let(:valid_attributes) { FactoryGirl.attributes_for(:product) }
+
+    it "is valid with all valid attributes" do
+      expect(Product.new(valid_attributes)).to be_valid
+    end
+
+    it "is invalid without width" do
+      product = FactoryGirl.build(:product, width: nil)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid without height" do
+      product = FactoryGirl.build(:product, height: nil)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid without depth" do
+      product = FactoryGirl.build(:product, depth: nil)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid without stock_level" do
+      product = FactoryGirl.build(:product, stock_level: nil)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid with width less than 1" do
+      product = FactoryGirl.build(:product, width: 0)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid with height less than 1" do
+      product = FactoryGirl.build(:product, height: 0)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid with depth less than 1" do
+      product = FactoryGirl.build(:product, depth: 0)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid with weight less than 1" do
+      product = FactoryGirl.build(:product, weight: 0)
+      expect(product).not_to be_valid
+    end
+
+    it "is invalid with stock level less than 1" do
+      product = FactoryGirl.build(:product, stock_level: 0)
+      expect(product).not_to be_valid
+    end
+  end
 end
