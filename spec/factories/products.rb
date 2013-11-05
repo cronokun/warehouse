1218a62c453e2233a29acb63c20da13123184d9f
Sergey Kviatkovsky <crono.neko@gmail.com>
2013-11-05 21:40:19 +0300

    Update product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,13 +1,17 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :product_number do |n|
+    "ABC #{rand(100..999)}#{n}00-#{rand(1000..9999)}-#{rand(10..99)}"
+  end
+
   factory :product do
-    product_number 1
-    name "MyString"
-    width 1
-    height 1
-    depth 1
-    weight 1
+    product_number
+    name { Faker::Commerce.product_name }
+    width 50
+    height 100
+    depth 50
+    weight 10
     stock_level 1
   end
 end
