76941c40ae91b1975d6a41bf66cab42fe782f926
Sergey Kviatkovsky <crono.neko@gmail.com>
2013-11-06 00:37:31 +0300

    Add validation for box model

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,2 +1,4 @@
 class Box < ActiveRecord::Base
+  validates :name, presence: true
+  validates :volume, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/box_spec.rb
+++ spec/models/box_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Box do
+  describe "new instance" do
+    it "is valid with all valid attributes" do
+      valid_attributes = FactoryGirl.attributes_for(:box)
+      expect(Box.new(valid_attributes)).to be_valid
+    end
+    
+    it "is invalid without name" do
+      expect(FactoryGirl.build(:box, name: nil)).not_to be_valid
+    end
+
+    it "is invalid without volume" do
+      expect(FactoryGirl.build(:box, volume: nil)).not_to be_valid
+    end
+
+    it "is invalid with volume less than 1" do
+      expect(FactoryGirl.build(:box, volume: 0)).not_to be_valid
+    end
+  end
 end
