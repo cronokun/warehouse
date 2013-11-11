2d16248c6dae72d752214d580b8cfc87962b76cb
Sergey Kviatkovsky <crono.neko@gmail.com>
2013-11-11 14:19:25 +0300

    Update styles on packing result page
    Also fix decorator error.

--- app/decorators/packed_box_decorator.rb
+++ app/decorators/packed_box_decorator.rb
@@ -2,14 +2,14 @@ class PackedBoxDecorator < Draper::Decorator
   delegate_all
 
   def filled_volume
-    h.content_tag(:p, "box filled #{ total_volume / 1_000_000.0 } of #{ volume_in_liters }")
+    h.content_tag(:strong, "box filled #{ object.filled_volume / 1_000_000.0 } of #{ volume_in_liters } liters:")
   end
 
   def list_of_products
     products = products_with_stock.map do |product, stock|
       h.content_tag(:li, "#{stock}&nbsp;&times;&nbsp;#{product.name}".html_safe)
     end
-    h.content_tag(:ul, h.safe_join(products))
+    h.content_tag(:ul, h.safe_join(products), class: "list-unstyled")
   end
 
 end
